19e0202d1dde5b603adc64474ff9aff19eacc802
Alex Karevoll <alexkarevoll@Alexs-MacBook-Pro-2.local>
2016-11-22 15:15:33 -0800

    usernames come along for the ride

--- server.js
+++ server.js
@@ -4,19 +4,26 @@ var
   http = require('http').Server(app),
   io = require('socket.io')(http)
 
+
 const PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function(socket) {
-  console.log("someone connected")
   socket.on('chat message', function(msg) {
     io.emit('push message', msg)
     console.log('message: ', msg)
   })
 })
+
+// io.on('connection', function(socket) {
+//     console.log("someone connected")
+//     io.emit('connection message', "A new user connected")
+// })
 
 http.listen(PORT, function() {
   console.log('listening on port', PORT)
